106e5ea9bb44b04c84ff2cdb8a3b0e56918e2221
Nikolay Mosalov <73695807+Rom-dev-tech@users.noreply.github.com>
2021-10-30 21:33:40 +0300

    fix joi validate

--- routes/api/contacts/joiSchema.js
+++ routes/api/contacts/joiSchema.js
@@ -1,9 +1,15 @@
 const Joi = require('joi')
 
 const joiSchema = Joi.object({
-  name: Joi.string().min(3).max(30).required(),
-  email: Joi.string().email().required(),
-  phone: Joi.string().max(15).required(),
+  name: Joi.string()
+    .min(3).max(30)
+    .pattern(/^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$/)
+    .required(),
+  email: Joi.string().email({ minDomainSegments: 2 }).required(),
+  phone: Joi.string()
+    .max(15)
+    .pattern(/\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}/)
+    .required(),
 })
 
 module.exports = joiSchema
