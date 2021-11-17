55e10cbe0d2e5fd06c67043077ee81d86864fdb3
Nikolay Mosalov <73695807+Rom-dev-tech@users.noreply.github.com>
2021-11-17 18:46:36 +0200

    fix updateStatusContact

--- controllers/contacts/updateStatusContact.js
+++ controllers/contacts/updateStatusContact.js
@@ -4,9 +4,23 @@ const { sendSuccessRes, notFound } = require('../../utils')
 const updateStatusContact = async (req, res, next) => {
   const { contactId } = req.params
   const { favorite } = req.body
+  const { email } = req.user
+
+  const validation = await Contact.findById(contactId).populate(
+    'owner',
+    '_id email'
+  )
+
+  if (!validation || email !== validation.owner.email) {
+    return notFound(contactId, next)
+  }
+
   const result = await Contact.findByIdAndUpdate(contactId, { favorite }, {
     new: true,
-  })
+  }).populate(
+    'owner',
+    '_id email'
+  )
 
   if (!result) {
     return notFound(contactId, next)
