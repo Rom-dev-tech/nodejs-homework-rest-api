0c69d3f9c6714843bd243f6c3abc27609bb02f95
Nikolay Mosalov <73695807+Rom-dev-tech@users.noreply.github.com>
2021-11-17 14:29:32 +0200

    fix getContactById

--- controllers/contacts/getContactById.js
+++ controllers/contacts/getContactById.js
@@ -3,9 +3,13 @@ const { sendSuccessRes, notFound } = require('../../utils')
 
 const getContactById = async (req, res, next) => {
   const { contactId } = req.params
-  const result = await Contact.findById(contactId)
+  const { email } = req.user
+  const result = await Contact.findById(contactId).populate(
+    'owner',
+    '_id email'
+  )
 
-  if (!result) {
+  if (!result || email !== result.owner.email) {
     return notFound(contactId, next)
   }
 
