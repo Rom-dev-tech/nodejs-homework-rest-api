ffe38f0e3ae80dfa9a3d0ed4d4be94db51396279
Nikolay Mosalov <73695807+Rom-dev-tech@users.noreply.github.com>
2021-11-18 12:07:29 +0200

    adds pagination and field filter favorite

--- controllers/contacts/listContacts.js
+++ controllers/contacts/listContacts.js
@@ -3,8 +3,28 @@ const { sendSuccessRes } = require('../../utils')
 
 const listContacts = async (req, res) => {
   const { _id } = req.user
+  const { page, limit, favorite } = req.query
 
-  const result = await Contact.find({ owner: _id }).populate('owner', '_id email')
+  const skip = (page - 1) * limit
+  const findBy = { owner: _id }
+  const requestedFields = '_id name email phone favorite owner'
+  const pagination = { skip, limit: Number(limit) }
+
+  if (favorite) {
+    findBy.favorite = favorite
+  }
+
+  if (!page && !limit) {
+    const result = await Contact.find(findBy, requestedFields)
+      .populate('owner', '_id email')
+
+    sendSuccessRes(res, { result })
+
+    return
+  }
+
+  const result = await Contact.find(findBy, requestedFields, pagination)
+    .populate('owner', '_id email')
 
   sendSuccessRes(res, { result })
 }
